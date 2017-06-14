3986321994e8e8d4f793600611216bbec4ed2baa
Sir_Josh <josh.moodley09@gmail.com>
2017-06-14 17:19:03 +0200

    Move travis script

---
+++ UI/app/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+app.debug = True
+
+@app.route('/')
+def main():
+    return 'Hi ! I\'m a Flask application. I was automatically tested and deployed using Travis.'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
